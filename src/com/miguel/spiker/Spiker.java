a1fbe564d9a69c9a1512a528c250cc0009225cf8
Miguel <Miguel@DESKTOP-876ELT2>
2017-01-05 22:59:23 -0500

    shooting ball

--- src/com/miguel/spiker/Spiker.java
+++ src/com/miguel/spiker/Spiker.java
@@ -1,9 +1,21 @@
 package com.miguel.spiker;
 
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+
 public class Spiker {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        LCD.drawString("Spiker", 0, 0);
+
+        EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+
+        motorA.setSpeed(1000);
+        motorA.rotate(-1800);
+
+
+        motorA.close();
 
     }
 
