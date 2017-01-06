17cdf43d75101785aa7e05380bbfc484454eb3bc
Miguel <Miguel@DESKTOP-876ELT2>
2017-01-05 21:50:02 -0500

    test

--- src/com/miguel/spiker/Spiker.java
+++ src/com/miguel/spiker/Spiker.java
@@ -2,9 +2,9 @@ package com.miguel.spiker;
 
 public class Spiker {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
